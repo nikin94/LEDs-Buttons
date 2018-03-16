f19f9c0387c1b3dd642e51183dac88e133b01473
Sergey Nikiforov <z.94@bk.ru>
2018-03-16 18:39:33 +0300

    not important changes

--- game.py
+++ game.py
@@ -5,9 +5,29 @@ from gpiozero import LED
 from gpiozero import Button as Button_gpio
 from tkinter import *
 
-leds = [LED(4),LED(15),LED(17),LED(22),LED(24),LED(6),LED(19),LED(20)]
-buttons = [Button_gpio(3),Button_gpio(14),Button_gpio(18),Button_gpio(27),Button_gpio(23),Button_gpio(5),Button_gpio(13),Button_gpio(16)]
-colors = ['red', 'orange', 'gold', 'green', 'cyan' ,'blue', 'violet']
+leds = [LED(4),
+        LED(15),
+        LED(17),
+        LED(22),
+        LED(24),
+        LED(6),
+        LED(19),
+        LED(20)]
+buttons = [Button_gpio(3),
+           Button_gpio(14),
+           Button_gpio(18),
+           Button_gpio(27),
+           Button_gpio(23),
+           Button_gpio(5),
+           Button_gpio(13),
+           Button_gpio(16)]
+colors = ['red',
+          'orange',
+          'gold',
+          'green',
+          'cyan',
+          'blue',
+          'violet']
 
 ###
 #leds.reverse()
